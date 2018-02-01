fbe7b879c1fc531239cec756253b7fdf582d3d90
David V <vudavid0@gmail.com>
2018-02-01 04:15:49 +0000

    add introduction and zoom pics

--- public/js/custom.js
+++ public/js/custom.js
@@ -11,6 +11,11 @@ $(document).ready(function () {
         }
     });
   
+    $('.zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
     
     
 });
